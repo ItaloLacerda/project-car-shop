acc60a5090ed12eabd4d9821b752d3c18e1ca19f
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-23 15:47:35 -0300

    feat: implement ErrorHandler in the app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import ErrorHandler from './middlewares/ErrorHandler';
 import CarRouter from './Router/CarRouter';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', CarRouter);
+app.use(ErrorHandler.handle);
 
 export default app;
