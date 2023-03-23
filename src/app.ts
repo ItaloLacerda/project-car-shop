86d7bf431ba5a7f2dfdbe24a0dc531be9e0aae20
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-23 17:50:02 -0300

    feat: implement route "/motorcycles"

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import ErrorHandler from './middlewares/ErrorHandler';
 import CarRouter from './Router/CarRouter';
+import MotorcycleRouter from './Router/MotorcycleRouter';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', CarRouter);
+app.use('/motorcycles', MotorcycleRouter);
 app.use(ErrorHandler.handle);
 
 export default app;
