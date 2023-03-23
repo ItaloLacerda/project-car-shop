3325e0f9aa7213c18459bcf262e19cc644782b25
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-23 19:21:40 -0300

    test: create carMocks

---
+++ tests/integration/mocks/carMocks.ts
@@ -0,0 +1,16 @@
+import ICar from '../../../src/Interfaces/ICar';
+
+const allCars: ICar[] = [
+  {
+    id: '6348513f34c397abcad040b2',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+];
+
+export default allCars;
