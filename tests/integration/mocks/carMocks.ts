141cdf89e22fe25d2138f936da326226599d52e8
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-24 18:18:38 -0300

    test: implement mocks in carMocks

--- tests/integration/mocks/carMocks.ts
+++ tests/integration/mocks/carMocks.ts
@@ -1,6 +1,6 @@
 import ICar from '../../../src/Interfaces/ICar';
 
-const allCars: ICar[] = [
+export const allCars: ICar[] = [
   {
     id: '6348513f34c397abcad040b2',
     model: 'Marea',
@@ -12,15 +12,34 @@ const allCars: ICar[] = [
     seatsQty: 5,
   },
   {
-    id: '6348513f34c397abcad040b2',
-    model: 'Marea',
-    year: 2002,
-    color: 'Black',
+    id: '6348513f34c397abcad040b3',
+    model: 'Gol',
+    year: 2010,
+    color: 'Green',
     status: true,
-    buyValue: 15.990,
-    doorsQty: 4,
-    seatsQty: 5,
+    buyValue: 30.990,
+    doorsQty: 2,
+    seatsQty: 6,
   },
 ];
 
-export default allCars;
+export const updateCar = {
+  id: '6348513f34c397abcad040b2',
+  model: 'Gol',
+  year: 2010,
+  color: 'Green',
+  status: true,
+  buyValue: 30.990,
+  doorsQty: 2,
+  seatsQty: 6,
+};
+
+export const postCar = {
+  model: 'Gol',
+  year: 2010,
+  color: 'Green',
+  status: true,
+  buyValue: 30.990,
+  doorsQty: 2,
+  seatsQty: 6,
+};
