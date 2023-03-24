9fb3d3a63e5d84bf69a385183d7b6d2d6a0133cf
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-24 19:02:32 -0300

    refactor: removes findById method from CarODM

--- src/Models/CarODM.ts
+++ src/Models/CarODM.ts
@@ -1,6 +1,5 @@
-import { isValidObjectId, Schema } from 'mongoose';
+import { Schema } from 'mongoose';
 import ICar from '../Interfaces/ICar';
-import Errors from '../Utils/erros/Erros';
 import AbstractODM from './AbstractODM';
 
 export default class CarODM extends AbstractODM<ICar> {
@@ -15,12 +14,5 @@ export default class CarODM extends AbstractODM<ICar> {
       seatsQty: { type: Number, required: true },
     });
     super(schema, 'Car');
-  }
-
-  public async findById(_id: string): Promise<ICar | null> {
-    if (!isValidObjectId(_id)) {
-      throw new Errors('422', 'Invalid mongo id');
-    }
-    return this.model.findById({ _id });
   }
 }
