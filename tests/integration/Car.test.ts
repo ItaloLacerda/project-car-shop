8f7d0cfba6abafe73b01e2f7e6cb331fdd7893ad
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-23 21:13:38 -0300

    test: successfully implements test to fetch car by id

--- tests/integration/Car.test.ts
+++ tests/integration/Car.test.ts
@@ -20,4 +20,16 @@ describe('Testa rota "/cars"', function () {
         done();
       });
   });
+
+  it('Verifica se ao um carro por id corretamente retorna o status 200', function (done) {
+    Sinon.stub(CarODM.prototype, 'findById').resolves(carMocks[0]);
+
+    supertest(app)
+      .get('/cars/6348513f34c397abcad040b2')
+      .expect(200)
+      .end(function (err, _res) {
+        if (err) done(err);
+        done();
+      });
+  });
 });
