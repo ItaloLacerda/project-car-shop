9e93c13a35d3f80d2d1f32e7f72246e8526e5718
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-23 21:14:23 -0300

    test[refactor]: refactor allCars

--- tests/integration/mocks/carMocks.ts
+++ tests/integration/mocks/carMocks.ts
@@ -11,6 +11,16 @@ const allCars: ICar[] = [
     doorsQty: 4,
     seatsQty: 5,
   },
+  {
+    id: '6348513f34c397abcad040b2',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
 ];
 
 export default allCars;
